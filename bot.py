377e23f0834b37bb8e514a5409402ed837ccc253
Rishav Kundu <rishav.kundu98@gmail.com>
2016-08-06 14:12:53 +0530

    allow pausing of bot

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 api = dota2api.Initialise("B57B59DCAED83EF462AC75019B4F0804")
 client = discord.Client()
 
+paused = False
+
 
 def get_games():
     resp = api.get_live_league_games()
@@ -70,41 +72,49 @@ def get_winner(mid):
 
 @client.event
 async def on_message(message):
+    global paused
     if message.content.startswith('!live'):
         await client.send_message(message.channel, describe_live_games())
+    elif message.content.startswith('!pause'):
+        paused = True
+        await client.send_message(message.channel, "The bot is paused.")
+    elif message.content.startswith('!resume, "The bot has been unpaused.'):
+        paused = False
 
 
 async def update_tracker(last_known):
-    
-    current = get_games()
-    msg = []
-    
-    print([x.get('match_id', -1) for x in last_known])
-    print(list([x['match_id'] for x in current]))
 
-    for i in range(len(last_known)):
-        if last_known[i] != {}:
-            mid = last_known[i]['match_id']
-            if mid not in [x['match_id'] for x in current]:
-                d = describe(last_known[i])
-                print(mid)
-                try:
-                    victor = get_winner(mid)
-                    msg.append("{} has finished. {} victorious!".format(d, victor))
-                except:
-                    pass
-                last_known[i] = {} # dirty hack
+    if not paused:
+        current = get_games()
+        msg = []
 
-    for i in current:
-        d = describe(i)
-        mid = i['match_id']
-        if mid not in [x.get('match_id', -1) for x in last_known]:
-            msg.append("{} has started!".format(d))
-            last_known.append(i)
+        print([x.get('match_id', -1) for x in last_known])
+        print(list([x['match_id'] for x in current]))
 
-    if msg:
-        await client.send_message(client.get_server("195955299482337281"),
-                            "\n".join(msg))
+        for i in range(len(last_known)):
+            if last_known[i] != {}:
+                mid = last_known[i]['match_id']
+                if mid not in [x['match_id'] for x in current]:
+                    d = describe(last_known[i])
+                    print(mid)
+                    try:
+                        victor = get_winner(mid)
+                        msg.append("{} has finished. {} victorious!".
+                                   format(d, victor))
+                    except:
+                        pass
+                    last_known[i] = {}  # dirty hack
+
+        for i in current:
+            d = describe(i)
+            mid = i['match_id']
+            if mid not in [x.get('match_id', -1) for x in last_known]:
+                msg.append("{} has started!".format(d))
+                last_known.append(i)
+
+        if msg:
+            await client.send_message(client.get_server("195955299482337281"),
+                                      "\n".join(msg))
 
     await asyncio.sleep(60)
     await update_tracker(last_known)
@@ -127,6 +137,7 @@ def describe_live_games():
 
 if __name__ == "__main__":
     _thread.start_new_thread(app.run, (),
-                             {"port": os.environ.get('PORT', 5000), 'host': '0.0.0.0'})
+                             {"port": os.environ.get('PORT', 5000),
+                              'host': '0.0.0.0'})
     # print("test")
     client.run('MjEwNDQwMTk1NDU4MzM0NzIw.CoO3oQ.aLq9tFdUv8QIO0l26vjy8PB8JMM')
